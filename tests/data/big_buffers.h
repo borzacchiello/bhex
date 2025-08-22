0beebad848919e574539ac9e0af95f834da33234
Luca Borzacchiello <luca.borzacchiello@nozominetworks.com>
2025-08-23 00:59:26 +0200

    rng test buffer: use custom rng
    use custom rng to be consistent accross different rand() implementations

--- tests/data/big_buffers.h
+++ tests/data/big_buffers.h
@@ -8,13 +8,23 @@
 static u8_t answer_to_universe[8000];
 static u8_t pseudo_random[8000];
 
+static unsigned long int __rng_next = 1;
+
+static int __rng_rand(void)
+{
+    __rng_next = __rng_next * 1103515243 + 12345;
+    return (unsigned int)(__rng_next / 65536) % 32768;
+}
+
+static void __rng_srand(unsigned int seed) { __rng_next = seed; }
+
 __attribute__((constructor)) static void __init_big_buffers(void)
 {
     memset(answer_to_universe, 0x42, sizeof(answer_to_universe));
 
-    srand(0xdeadbeef);
+    __rng_srand(0xdeadbeef);
     for (size_t i = 0; i < sizeof(pseudo_random); i++) {
-        pseudo_random[i] = rand() % ('Z' - 'A' + 1) + 'A';
+        pseudo_random[i] = __rng_rand() % ('Z' - 'A' + 1) + 'A';
     }
 }
 
