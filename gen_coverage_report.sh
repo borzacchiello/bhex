6a1a2883fa53c147d76ae0c947b7491204e0dc2f
Luca Borzacchiello <lucaborza@gmail.com>
2025-08-24 15:32:29 +0200

    tests: add coverage info

---
+++ gen_coverage_report.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+mkdir ./build_coverage
+
+cd ./build_coverage
+cmake -DENABLE_TESTS=on -DCMAKE_BUILD_TYPE=Debug -DASAN=on ..
+make -j
+
+# I don't know why it complains about these files...
+cp ../bhengine/lexer.* .
+cp ../bhengine/parser.* .
+
+ctest -T Coverage -T Test
+lcov --directory . --capture --output-file coverage.info
+genhtml --demangle-cpp -o coverage coverage.info
+open coverage/index.html
